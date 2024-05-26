10a4539daaffb33ba1fb44a722f8db49e77bf219
user <katya.rotar02@gmail.com>
2024-05-26 17:19:06 +0300

    Lab 6.4 Fixed DemoApplicationTests by adding test containers.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
+@ActiveProfiles("spring-boot-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	@TestConfiguration
+	static class TestConfig {
+		@Bean
+		public DataSource dataSource() {
+			return DataSourceBuilder.create()
+					.driverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver")
+					.url("jdbc:tc:postgresql:12:///recipeBook_db?TC_TMPFS=/testtmpfs:rw")
+					.username("user")
+					.password("password")
+					.build();
+		}
+	}
 }
