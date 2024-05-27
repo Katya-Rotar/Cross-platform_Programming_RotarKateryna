4f2efa68846b74da9ffdcc49742a356ea38bd001
user <katya.rotar02@gmail.com>
2024-05-27 23:28:52 +0300

    Lab 7.3 Added configuration property for items per page. Modified RecipeController to retrieve recipes with pagination. Modified recipe list template to display the first 10 recipes.

--- src/main/java/com/example/demo/recipes/RecipesController.java
+++ src/main/java/com/example/demo/recipes/RecipesController.java
@@ -1,5 +1,8 @@
 package com.example.demo.recipes;
 
+import com.example.demo.recipeBook.recipe.RecipeService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,15 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping("/recipes")
 public class RecipesController {
-
+    private final RecipeService recipeService;
+    public RecipesController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
     @GetMapping
-    public String projects(Model model) {
+    public String projects(Model model, @SortDefault.SortDefaults({
+            @SortDefault("title")}) Pageable pageable) {
+        model.addAttribute("titleRecipe", "Recipes");
+        model.addAttribute("recipes", recipeService.getRecipes(pageable));
         return "recipes/recipes";
     }
 }
