6d481ddb563c671898b2203c448bb0abb6658a59
user <katya.rotar02@gmail.com>
2024-05-28 21:45:10 +0300

    Lab 8.2 Extended controller to redirect to create form and back

--- src/main/java/com/example/demo/addRecipe/AddRecipeController.java
+++ src/main/java/com/example/demo/addRecipe/AddRecipeController.java
@@ -2,9 +2,10 @@ package com.example.demo.addRecipe;
 
 import com.example.demo.recipeBook.recipe.Category;
 import com.example.demo.recipeBook.recipe.Difficulty;
-import com.example.demo.recipeBook.recipe.Recipe;
+import com.example.demo.recipeBook.recipe.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -14,7 +15,10 @@ import java.util.List;
 @Controller
 @RequestMapping("/addRecipe")
 public class AddRecipeController {
-
+    private final RecipeService recipeService;
+    public AddRecipeController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
     @GetMapping
     public String showAddRecipeForm(Model model) {
         model.addAttribute("recipe", new CreateRecipeFormData());
@@ -26,7 +30,9 @@ public class AddRecipeController {
     }
 
     @PostMapping
-    public String submitRecipeForm(Recipe recipe, Model model) {
+    public String submitRecipeForm(@ModelAttribute("recipe") CreateRecipeFormData formData,
+                                   Model model) {
+        recipeService.createRecipe(formData.toParameters());
         return "redirect:/recipes";
     }
 }
